8e558d22a71a4f8b1c2969156d818f16ab9ad2e7
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-08 17:00:56 +0300

    Added models (#4)

---
+++ MovieDB/Models/MovieModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct MovieModel: Identifiable, Codable {
+  let poster: Poster
+  let rating: Rating
+  let id: Int
+  let movieLength: Int
+  let name: String
+  let shortDescription: String?
+  let year: Int
+}
+
+struct Poster: Codable {
+  let id: String
+  let url, previewURL: String
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+    case url
+    case previewURL = "previewUrl"
+  }
+}
+
+struct Rating: Codable {
+  let id: String
+  let kp, imdb: Double
+  let filmCritics, russianFilmCritics, await: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+    case kp, imdb, filmCritics, russianFilmCritics, await
+  }
+}
