537c7937f44232dd973ea21083b198fbfd473eab
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-08 20:01:08 +0300

    Selected movie (#10)

--- MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
+++ MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
@@ -1,7 +1,9 @@
 import SwiftUI
 
 struct SelectedMovieScreen: View {
+  let video: Video
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    Text(video.name)
   }
 }
