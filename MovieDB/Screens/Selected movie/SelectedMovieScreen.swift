425fd54665a023a67a2f34fc1fabf101137a9fc8
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-12 18:03:18 +0300

    Add async image to selected screen (#15)

--- MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
+++ MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
@@ -2,8 +2,15 @@ import SwiftUI
 
 struct SelectedMovieScreen: View {
   let video: Video
+  let defaultUrl: String = "https://fikiwiki.com/uploads/posts/2022-02/1645054777_3-fikiwiki-com-p-kartinki-ikonki-3.png"
+  let imgUrl: URL
+  
+  init(video: Video) {
+    self.video = video
+    self.imgUrl = URL(string: video.poster.url) ?? URL(string: defaultUrl)!
+  }
   
   var body: some View {
-    Text(video.name)
+    AsyncImage(url: imgUrl)
   }
 }
