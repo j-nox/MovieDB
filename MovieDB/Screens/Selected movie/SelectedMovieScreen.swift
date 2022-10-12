0e6ec39763638262503d6b1d32a507529d9ce2b1
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-12 18:56:42 +0300

    Added stub image to selected movie screen (#17)

--- MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
+++ MovieDB/Screens/Selected movie/SelectedMovieScreen.swift
@@ -2,15 +2,24 @@ import SwiftUI
 
 struct SelectedMovieScreen: View {
   let video: Video
-  let defaultUrl: String = "https://fikiwiki.com/uploads/posts/2022-02/1645054777_3-fikiwiki-com-p-kartinki-ikonki-3.png"
+  let defaultUrl: String = "https://fikiwikiee.com/uploads/posts/2022-02/1645054777_3-fikiwiki-com-p-kartinki-ikonki-3.png"
   let imgUrl: URL
   
   init(video: Video) {
     self.video = video
-    self.imgUrl = URL(string: video.poster.url) ?? URL(string: defaultUrl)!
+    self.imgUrl = URL(string: video.poster.url)!
   }
   
   var body: some View {
-    AsyncImage(url: imgUrl)
+    AsyncImage(url: imgUrl) { phase in
+      if let image = phase.image {
+        image
+      } else if phase.error != nil {
+        Image("stubImage")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 190, height: 190)
+      }
+    }
   }
 }
